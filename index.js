876faffeea49fafeaef7c400fb9cefefa288a28b
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2022-04-08 10:00:06 +0530

    port fixed

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(5000, () => {
-  console.log("app now listening for requests on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("app now listening for requests on port " + PORT);
 });
