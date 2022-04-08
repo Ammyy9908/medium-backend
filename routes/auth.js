3e8f0df5d725c701f1cc406b61cf75d407031a9a
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2022-04-08 10:53:19 +0530

    facebook added5

--- routes/auth.js
+++ routes/auth.js
@@ -62,7 +62,7 @@ router.get(
   passport.authenticate("google"),
   async (req, res) => {
     const token = await jwt.sign(
-      { id: req.user.google_id },
+      { id: req.user.social_id },
       process.env.APP_SECRET
     );
     res.redirect("exp://192.168.1.2:19000?token=" + token + "&provider=google");
