120ad7f6d28534d8d79503f5ead67c256f8bfd2c
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2022-04-08 12:38:47 +0530

    optional4

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authCheck = (req, res, next) => {
   if (!req.user) {
-    res.redirect("/auth/login");
+    res.send("You have to login first");
   } else {
     next();
   }
